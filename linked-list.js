c021a62250ecf6407cc058f0ce8207b34ac640d3
Edmond Soun <edmond.soun@gmail.com>
2022-09-19 14:59:16 -0700

    adds methods on LinkedList class

--- linked-list.js
+++ linked-list.js
@@ -24,17 +24,58 @@ class LinkedList {
 
   push(val) {
 
+    let newNode = new Node(val);
+    if (this.head === null) this.head = newNode;
+    if (this.tail !== null) this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
 
+    let newNode = new Node(val);
+
+    //if we have a head:
+    newNode.next = this.head;
+    this.head = newNode;
+
+    if (this.tail === null) this.tail = newNode;
+
+    this.length++;
+
   }
 
   /** pop(): return & remove last item. */
+  //Throw error if list is empty
 
   pop() {
+    if (this.length === 0) throw new Error;
+
+    let poppedVal = this.head.val;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return poppedVal;
+    }
+
+    let current = this.head;
+    //traverse the array until we reach the second to last element
+    //make this element the tail
+    //return the origin last value
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 2) {
+        poppedVal = current.next.val;
+        this.tail = current;
+        this.length--;
+      }
+    }
+
+    return poppedVal;
 
   }
 
@@ -71,7 +112,7 @@ class LinkedList {
   /** average(): return an average of all values in the list */
 
   average() {
-    
+
   }
 }
 
